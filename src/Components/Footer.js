25562647f74d70d969754f71dbccfc6701e40f78
rohan patil <rohanpatil9904@gmail.com>
2024-09-21 00:04:53 +0530

    attached quick link in footer page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,10 +18,10 @@ const Footer = () => {
         <div>
           <h1 className="text-2xl font-semibold mb-4">Quick Links</h1>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-gray-300">Home</a></li>
-            <li><a href="#" className="hover:text-gray-300">About</a></li>
-            <li><a href="#" className="hover:text-gray-300">Services</a></li>
-            <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+            <li><a href="/" className="hover:text-gray-300">Home</a></li>
+            <li><a href="/About" className="hover:text-gray-300">About</a></li>
+            <li><a href="/Donate" className="hover:text-gray-300">Services</a></li>
+            <li><a href="/Contact" className="hover:text-gray-300">Contact</a></li>
           </ul>
         </div>
 
